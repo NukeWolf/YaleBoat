eaf85f2b4b36154ce7cb05a755ea0bb40337ff8e
NukeWolf <smart.alex.huang@gmail.com>
2021-02-18 01:52:39 -0500

    Added clearchannels command

---
+++ commands/clearchess.js
@@ -0,0 +1,27 @@
+const {mainGuild,chessCategory} = require('../config')
+module.exports = {
+    name: 'clearchess',
+    aliases:[],
+    description: '',
+    permissions: ['ADMINISTRATOR'], 
+    /**
+     * @param  {import('discord.js').Message} message
+     * @param  {Array<String>} args
+     */
+    async execute(message,args) {
+        const guild = message.client.guilds.resolve(mainGuild)
+        const category = guild.channels.resolve(chessCategory)
+        /**
+         * @param  {import('discord.js').TextChannel} channel
+         */
+        const setup = async (channel) =>{
+            const unix = channel.name.match(/[\d]+$/g)
+            const role = guild.roles.cache.find((role) =>{
+                return role.name == `chess${unix}`
+            })
+            role.delete()
+            channel.delete()
+        }
+        category.children.forEach(setup)
+    }
+}
