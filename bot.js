03b0d3375b314df37415442719a271b42cd1de35
NukeWolf <smart.alex.huang@gmail.com>
2021-04-08 12:53:34 -0400

    changed reaction emoji

--- bot.js
+++ bot.js
@@ -149,7 +149,7 @@ client.on("guildMemberAdd", async (member) => {
 
 client.on("message", async (message) => {
     if (message.content.toLowerCase().includes("harvard"))
-        return message.react("😠");
+        return message.react("😞");
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
