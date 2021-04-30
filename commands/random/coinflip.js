c769e8403ad77b8432b75e5708eddf96a5c6c951
NukeWolf <smart.alex.huang@gmail.com>
2021-04-30 11:10:51 -0400

    Updated EventData, added coinflip alias

--- commands/random/coinflip.js
+++ commands/random/coinflip.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 
 module.exports = {
     name: "coinflip",
-    aliases: ["coin", "flip"],
+    aliases: ["coin", "flip", "flipcoin"],
     description: "Flips a coin.",
     execute(message, args) {
         return message.reply(_.sample(["Heads", "Tails"]));
