d9a68b1e201f2ad0420ef7feabecba1b9f3dca75
NukeWolf <smart.alex.huang@gmail.com>
2021-07-10 13:02:44 -0400

    Removed angad detection

--- bot.js
+++ bot.js
@@ -160,8 +160,8 @@ client.on("message", async (message) => {
     if (message.content.toLowerCase().includes("yale"))
         message.react("797522900965392395");
     if (message.content.toLowerCase().includes("harvard")) message.react("😞");
-    if (message.member.id == "830234142062280744")
-        reactAngad(message, angadText);
+    // if (message.member?.id == "830234142062280744")
+    //     reactAngad(message, angadText);
     /*if (message.member.id == "754425089931608115")
         reactAngad(message, kelechiText);*/
 
